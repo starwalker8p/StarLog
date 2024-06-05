e6feda38ef7a20d12deb15b39a82362b6b623879
starwalker8p <starwalker8p@gmail.com>
2024-06-05 14:01:38 -0300

    sla

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-function MyButton() {
-  return(<button>I'm a button</button>);
+const Txtlink = (txt) =>{
+  return <a id="txtlink" href="#"><h5>{txt}</h5></a>
+}
+function SearchBar(gameN){
+  return <div className="searchbar">
+    <h5 id="searchtxt">Pesquisar</h5>
+    <img src="" alt="ic"/>
+  </div>
 }
 export default function MyApp(){
-  return(<div>
-    <h1>Welcome to my app</h1>
-    <MyButton/>
-  </div>)
+  return(
+    <div className='container'>
+    <nav>
+      {Txtlink("Entrar")}
+      {Txtlink("Entrar")}
+      {Txtlink("Jogos")}
+      {SearchBar()}
+    </nav>
+  </div>
+  );
 }
